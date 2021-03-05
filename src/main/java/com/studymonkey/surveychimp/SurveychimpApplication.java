e924815d8246df0730bf2b7a189304caf696b798
Rahul <rahulanilkumar8@gmail.com>
2021-03-05 09:39:16 -0500

    Project Initialization

---
+++ src/main/java/com/studymonkey/surveychimp/SurveychimpApplication.java
@@ -0,0 +1,13 @@
+package com.studymonkey.surveychimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveychimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveychimpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studymonkey/surveychimp/SurveychimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studymonkey.surveychimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveychimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

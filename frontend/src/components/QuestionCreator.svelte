02cd7b6360b4e5e75a8c9b5cf44ed50c337e0d90
Rahul <rahulanilkumar8@gmail.com>
2021-03-11 11:08:41 -0500

    Removed commented out code

--- frontend/src/components/QuestionCreator.svelte
+++ frontend/src/components/QuestionCreator.svelte
@@ -1,6 +1,3 @@
-<!-- This page is currently a template for the Question. It is a playground for other developers to play around with some
-svelte features. -->
-
 <script>
     let answerType = "TEXT";
     let survey_id;
@@ -9,20 +6,6 @@ svelte features. -->
         mcQuestion = mcQuestion.concat({surveyId: surveyId, question: questionTxt, questionType: qType, mcOption:[]});
     }
 </script>
-
-
-<!--This code can be used as a template for determining question type using checkboxes-->
-<!--<div>-->
-<!--    <h2>What type of answer would you like for this question?</h2>-->
-<!--    <label>-->
-<!--        <input type=radio bind:group={answerType} value={"MULTIPLE_CHOICE"}>-->
-<!--        Multiple Choice-->
-<!--    </label>-->
-<!--    <label>-->
-<!--        <input type=radio bind:group={answerType} value={"TEXT"}>-->
-<!--        Text-->
-<!--    </label>-->
-<!--</div>-->
 
 <div class="mcQuestion">
     <label>

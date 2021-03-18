5ea8868967bea1ca9843cf701abcebe40934835e
Rahul <rahulanilkumar8@gmail.com>
2021-03-18 02:05:27 -0400

    issue#32 and survey viewing

--- src/main/java/com/studymonkey/surveychimp/entity/survey/Survey.java
+++ src/main/java/com/studymonkey/surveychimp/entity/survey/Survey.java
@@ -1,17 +1,32 @@
 package com.studymonkey.surveychimp.entity.survey;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * A survey's details
  *
  * Note: the ID is auto incremented by the database, therefore it is not used
  * when inserting the record into the database.
  */
+@Entity
 public class Survey {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String description;
     private SurveyStatus status;
+
+    public Survey (SurveyStatus status){
+        this.status = status;
+    }
+
+    public Survey() {
+    }
 
     public int getId() {
         return id;
@@ -38,7 +53,14 @@ public class Survey {
     }
 
     public int getStatus() {
+        if(status == null){
+            return SurveyStatus.CLOSED.getValue();
+        }
         return status.getValue();
+    }
+
+    public void setStatus(int status) {
+        this.setStatus(SurveyStatus.values()[status]);
     }
 
     public void setStatus(SurveyStatus status) {

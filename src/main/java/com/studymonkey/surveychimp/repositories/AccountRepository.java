846834f45df0ded09c83a8ad3a8c0755d97b6601
Thomas <32366586+Thomas-Leung@users.noreply.github.com>
2021-03-17 23:28:49 -0400

    clarify AccountRepository

--- src/main/java/com/studymonkey/surveychimp/repositories/AccountRepository.java
+++ src/main/java/com/studymonkey/surveychimp/repositories/AccountRepository.java
@@ -4,8 +4,15 @@ import com.studymonkey.surveychimp.entity.Account;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "userApi", path = "userApi")
+/*
+ * collectionResourceRel: show all accounts named accounts json
+ * itemResourceRel: show an account named account json
+ * path: the path - e.g. http://localhost:8080/accountApi
+ */
+@RepositoryRestResource(collectionResourceRel = "accounts", itemResourceRel = "account", path = "accountApi")
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
+    // Spring JPA built in
+    // GET request example: http://localhost:8080/accountApi/2
     Account findById(long id);
 }

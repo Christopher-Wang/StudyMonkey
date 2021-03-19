d9669e47c196e3f9719e2116879bdfb390e94e63
Christopher-Wang <christopher.jj.wang@gmail.com>
2021-03-19 19:09:48 -0400

    Added Question Create Form and links

--- src/main/java/com/studymonkey/surveychimp/viewControllers/QuestionController.java
+++ src/main/java/com/studymonkey/surveychimp/viewControllers/QuestionController.java
@@ -1,5 +1,6 @@
 package com.studymonkey.surveychimp.viewControllers;
 
+import com.studymonkey.surveychimp.entity.questions.McQuestion;
 import com.studymonkey.surveychimp.entity.questions.Question;
 import com.studymonkey.surveychimp.entity.questions.QuestionType;
 import com.studymonkey.surveychimp.entity.questions.TextQuestion;
@@ -25,7 +26,29 @@ public class QuestionController {
         this.surveyRepository = surveyRepository;
     }
 
+    /**
+     * Example Request: http://localhost:8080/question
+     * @param model the model of the system
+     * @return the view for creating a survey
+     */
+    @GetMapping
+    public String surveyForm(@RequestParam(value = "surveyId", required=false) Long surveyId, Model model) {
+        if (surveyId == null) surveyId = new Long(0);
+        model.addAttribute("surveyId", surveyId);
+        model.addAttribute("question", new Question());
+        return "questioncreate";
+    }
 
+    /**
+     * Example Request: http://localhost:8080/question
+     * @param model the model of the system
+     * @return the view for creating a survey
+     */
+    @PostMapping
+    public String test(@ModelAttribute("surveyId") long surveyId) {
+        System.out.print("\n\n\n\n" + surveyId + "\n\n\n\n");
+        return "questioncreate";
+    }
 
     @GetMapping("/question/{id}")
     @ResponseBody
@@ -39,10 +62,12 @@ public class QuestionController {
     public Survey postQuestion(@PathVariable long id, @RequestBody Object body) {
         Survey s = this.surveyRepository.findById(id);
 
-        Question q = new TextQuestion("I am Question", QuestionType.TEXT);
+        Question q = new McQuestion("I am Question", QuestionType.MULTIPLE_CHOICE);
+
         if (s == null){
             return null;
         }
+
         q.setSurvey(s);
         s.addQuestion(q);
         this.questionRepository.save(q);

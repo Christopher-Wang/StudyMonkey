8855935a4d6bc7d19dbae5f8b9f9fe58b4245b4c
Rahul <rahulanilkumar8@gmail.com>
2021-03-25 19:40:58 -0400

    made some changes to fix minor page transition bugs

--- src/main/java/com/studymonkey/surveychimp/viewControllers/ViewController.java
+++ src/main/java/com/studymonkey/surveychimp/viewControllers/ViewController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+    @GetMapping("/") // map request to method
+    public String DefaultPage() {
+        return "redirect:/login";
+    }
+
     @GetMapping("home") // map request to method
     public String homePage(Model model) {
         return "index";

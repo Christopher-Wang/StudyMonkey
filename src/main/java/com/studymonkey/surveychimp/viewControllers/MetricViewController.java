72c010550e054441e97487267874c60dd0f76287
Michael Patsula <Michael.Patsula@gmail.com>
2021-03-26 09:37:45 -0400

    Remove unused imports

--- src/main/java/com/studymonkey/surveychimp/viewControllers/MetricViewController.java
+++ src/main/java/com/studymonkey/surveychimp/viewControllers/MetricViewController.java
@@ -1,19 +1,13 @@
 package com.studymonkey.surveychimp.viewControllers;
 
-import com.studymonkey.surveychimp.entity.Account;
-import com.studymonkey.surveychimp.entity.answers.Answer;
-import com.studymonkey.surveychimp.entity.questions.Question;
-import com.studymonkey.surveychimp.repositories.AccountRepository;
 import com.studymonkey.surveychimp.repositories.MetricRepository;
 import com.studymonkey.surveychimp.repositories.QuestionRepository;
-import com.studymonkey.surveychimp.service.QueryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "metrics")

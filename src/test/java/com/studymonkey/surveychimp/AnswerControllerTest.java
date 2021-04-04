3160fbf11e9a8dc8b3d69a14de7b355c25c6c232
ChristopheTran <christophetran01@gmail.com>
2021-04-04 14:16:54 -0400

    added test cases for answering range questions

--- src/test/java/com/studymonkey/surveychimp/AnswerControllerTest.java
+++ src/test/java/com/studymonkey/surveychimp/AnswerControllerTest.java
@@ -6,6 +6,7 @@ import com.studymonkey.surveychimp.entity.answers.McAnswer;
 import com.studymonkey.surveychimp.entity.answers.TextAnswer;
 import com.studymonkey.surveychimp.entity.questions.QuestionType;
 import com.studymonkey.surveychimp.entity.wrapper.McQuestionWrapper;
+import com.studymonkey.surveychimp.entity.wrapper.RangeQuestionWrapper;
 import com.studymonkey.surveychimp.entity.wrapper.TextQuestionWrapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,7 @@ public class AnswerControllerTest {
     @BeforeAll
     public void setup() throws Exception {
         // Create the Survey first
+        // Survey will have id = 1
         mockMvc.perform(post("/surveyV2/surveycreation")
                 .param("name","Test Survey #3")
                 .param("description","This is a test")
@@ -53,14 +55,23 @@ public class AnswerControllerTest {
 
         // Mc question will have id = 3
         ArrayList<String> options = new ArrayList<String>();
-        options.add("Option 1");
-        options.add("Option 2");
+        options.add("Option 1"); // McOption will have id = 4
+        options.add("Option 2"); // McOption will have id = 5
         McQuestionWrapper wrapperMc = new McQuestionWrapper(1, "Test MC Question", QuestionType.MULTIPLE_CHOICE);
         wrapperMc.setOptions(options);
         String questionMcJSON = asJsonString(wrapperMc);
         mockMvc.perform(post("/question")
                 .param("questionType", "MULTIPLE_CHOICE")
                 .content(questionMcJSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        // Range question will have id = 6
+        RangeQuestionWrapper wrapperRange = new RangeQuestionWrapper(0,10,1L, "Test Range Question", QuestionType.RANGE);
+        String questionRangeJSON = asJsonString(wrapperRange);
+        mockMvc.perform(post("/question")
+                .param("questionType", "RANGE")
+                .content(questionRangeJSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
@@ -84,6 +95,15 @@ public class AnswerControllerTest {
     }
 
     @Test
+    public void addRangeAnswer() throws Exception {
+
+        mockMvc.perform(post("/answer/rangeAnswer/6")
+                .param("rangeAnswer","5")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     public void getQuestionAnswers() throws Exception {
         // Get answer for text question (id=2)
         this.mockMvc.perform(MockMvcRequestBuilders
@@ -94,6 +114,12 @@ public class AnswerControllerTest {
         // Get answer for mc question (id=3)
         this.mockMvc.perform(MockMvcRequestBuilders
                 .get("/answer/questionAnswers/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        // Get answer for range question (id=6)
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/answer/questionAnswers/6"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }

0c666857facee1c4a499e88d63295bc7d8ada64f
Christopher-Wang <christopher.jj.wang@gmail.com>
2021-04-08 01:42:18 -0400

    Initial Commit

--- src/main/java/com/studymonkey/surveychimp/repositories/AccountRepository.java
+++ src/main/java/com/studymonkey/surveychimp/repositories/AccountRepository.java
@@ -1,11 +1,12 @@
 package com.studymonkey.surveychimp.repositories;
 
 import com.studymonkey.surveychimp.entity.Account;
-import javassist.bytecode.ByteArray;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 /*
  * collectionResourceRel: show all accounts named accounts json
